cf1cea3526c91c6db9068d191880853952a2e327
Serg1 <spmm3113@gmail.com>
2021-03-16 09:57:03 +0100

    Mòdul 2- 1r push
    Signed-off-by: Serg1 <spmm3113@gmail.com>

---
+++ Modul2/src/Nivell1FaseI.java
@@ -0,0 +1,15 @@
+
+public class Nivell1FaseI {
+
+	public static void main(String[] args) {
+		
+		/*Crea una taula (char[]) amb el teu nom on cada posició correspongui a una lletra. 
+		Fes un bucle que recorri aquesta taula i mostri per consola cadascuna de les lletres.*/ 	
+		char nom[] ={'S','e','r','g','i'};
+		
+		for (int i = 0; i < nom.length; i++) {
+			System.out.println(nom[i]);
+		}			
+	}
+}
+
---
+++ Modul2/src/Nivell1FaseII.java
@@ -0,0 +1,13 @@
+
+public class Nivell1FaseII {
+	/* Canvia la taula per una llista (List<Character>) 
+	Al bucle, si la lletra és una vocal imprimeix a la consola: ‘VOCAL’. Sinó, imprimeix: ‘CONSONTANT’. 
+	Si trobes un numero, mostra per pantalla: ‘Els noms de persones no contenen números!*/
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
